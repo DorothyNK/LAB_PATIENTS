241507d591c565a7c999df30b51835b76bf3b1fd
unknown <dorothykotey66@gmail.com>
2023-12-18 08:43:49 -0800

    messgae

---
+++ modelsroutescontrollers/Encounter.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+
+const encounterSchema = new mongoose.Schema({
+  patientId: { type: mongoose.Schema.Types.ObjectId, ref:Patient, required: true },
+  dateofEncounter: { type: Date, default: Date.now },
+  timeofEncounter: String, 
+  typeofEncounter: String, 
+
+
+});
+
+const encounter = mongoose.model('encounter', encounterSchema);
+
+module.exports = encounter;
